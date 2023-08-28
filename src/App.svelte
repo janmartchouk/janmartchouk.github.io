a42840842ee5429d21c391f984eb60082bf1fe7f
najan <jan@martchouk.com>
2023-08-28 22:28:02 +0200

    Website commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,24 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Button from './lib/Button.svelte';
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+
+  <div class="center-container">
+
+    <h1 id="name"><span class="name-word" id="firstname">Jan</span> <span class="name-word" id="lastname">Martchouk</span></h1>
+
+    <Button id="insta" link="https://www.instagram.com/janmartchouk/"></Button>
+    <Button id="graphics" link="https://farbigstefarben.tumblr.com/"></Button>
+    <Button id="plants" link="https://pollywoodgreenhouse.tumblr.com/"></Button>
+    <Button id="pics" link="https://adidasfeetpics.tumblr.com/"></Button>
+    <Button id="towers" link="https://tuermeinstaedten.tumblr.com/"></Button>
+
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
---
+++ src/lib/Button.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let text = "button";
+    export let link = "#";
+    export let icon = "▸"
+    export let id = "button";
+
+    var buttonStrings: Record<string, Record<string, string>> = {
+    "en": {
+      "insta": "instagram & threads",
+      "graphics": "graphics",
+      "plants": "plants",
+      "pics": "pics",
+      "towers": "towers"
+    },
+    "de": {
+      "insta": "instagram & threads",
+      "graphics": "grafiken",
+      "plants": "pflanzen",
+      "pics": "bilder",
+      "towers": "türme"
+    }
+  }
+
+    let userLang:string = navigator.language;
+    if (userLang == "de" || userLang == "de-DE") {
+        text = buttonStrings.de[id];
+    } else {
+        text = buttonStrings.en[id];
+    }
+    
+    console.log(userLang)
+
+</script>
+
+<a role="button" class="button" id={id} href={link}>
+    <span class="button-icon">{icon}</span>{text}
+</a>
